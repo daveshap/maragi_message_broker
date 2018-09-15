8aa8fa9a3220761c019ab77a4ad67843e72eedf9
David Shapiro <daveshap37@gmail.com>
2018-09-15 07:33:47 -0400

    Update rabbit.sh

--- rabbit.sh
+++ rabbit.sh
@@ -1,1 +1,1 @@
-docker run -d --network maragi_net  --hostname MaragiRabbit --name MaragiRabbit -p 8080:15672 rabbitmq:3-management
+docker run -d --network maragi_net --hostname MaragiRabbit --name MaragiRabbit -p 8080:15672 rabbitmq:3-management
